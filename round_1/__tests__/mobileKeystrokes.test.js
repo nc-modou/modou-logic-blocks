ed9312c47012f7ba5f83e06d754af50f92f99639
nc-modou <modou.robinson@northcoders.com>
2024-04-25 09:13:28 +0100

    complete solution & add extra test

--- round_1/__tests__/mobileKeystrokes.test.js
+++ round_1/__tests__/mobileKeystrokes.test.js
@@ -1,6 +1,7 @@
 const { mobileDisplayKeystrokes } = require("../mobileKeystrokes");
 
 describe("mobileDisplayKeystrokes", () => {
+  const startTime = performance.now();
   test("should return 0 for an empty string", () => {
     expect(mobileDisplayKeystrokes("")).toBe(0);
   });
@@ -14,9 +15,14 @@ describe("mobileDisplayKeystrokes", () => {
     expect(mobileDisplayKeystrokes("abc")).toBe(9);
   });
   test("should return accurate keystroke count for longer words", () => {
-    expect(mobileDisplayKeystrokes("northcoders")).toBe(27);
+    expect(mobileDisplayKeystrokes("northcoders")).toBe(38);
   });
   test("should return accurate keystroke count for a combination string", () => {
-    expect(mobileDisplayKeystrokes("*#123northcoders")).toBe(32);
+    expect(mobileDisplayKeystrokes("*#123northcoders")).toBe(43);
   });
+  test("should return accurate keystroke count for a word with letters after w", () => {
+    expect(mobileDisplayKeystrokes("wxyz")).toBe(14);
+  });
+  const endTime = performance.now();
+  console.log(endTime - startTime);
 });
--- round_1/mobileKeystrokes.js
+++ round_1/mobileKeystrokes.js
@@ -1,62 +1,43 @@
 const mobileDisplayKeystrokes = (word) => {
   let keystrokes = 0;
+
+  const ASCII_CODE_OF_O = 111;
+  const ASCII_CODE_OF_V = 118;
+
   for (let i = 0; i < word.length; i++) {
-    console.log(keystrokes);
-    console.log(word[i - 1]);
     const asciiCode = word.charCodeAt(i);
-    console.log(`ASCII code of '${word[i]}' is: ${asciiCode}`);
+
     if (asciiCode >= 35 && asciiCode <= 57) {
       keystrokes++;
-    } else if (asciiCode <= 111) {
+    } else if (asciiCode <= ASCII_CODE_OF_O) {
       let asciiFactor = asciiCode % 3;
       if (asciiFactor === 1) {
-        console.log(`${word[i]} gives 2 keystrokes`);
         keystrokes += 2;
       } else if (asciiFactor === 2) {
-        console.log(`${word[i]} gives 3 keystrokes`);
         keystrokes += 3;
       } else if (asciiFactor === 0) {
-        console.log(`${word[i]} gives 4 keystrokes`);
         keystrokes += 4;
       }
-    } else if (asciiCode >= 111) {
-      // beyond o
-      let asciiFactor = asciiCode % 3;
-      console.log(`${word[i]} with code ${asciiCode} gets into the 110 range `);
-      console.log(`it's factor is ${asciiFactor}`);
-      let asciiFactorBy4 = asciiCode % 4;
-      if (asciiCode <= 118) {
-        console.log(`${word[i]} gets into the 118 block`);
-        console.log(`${asciiCode}`);
-        console.log(`ascii factor by 4 is ${asciiFactorBy4}`);
+    } else if (asciiCode >= ASCII_CODE_OF_O) {
+      const asciiFactorBy4 = asciiCode % 4;
+      if (asciiCode <= ASCII_CODE_OF_V) {
         if (asciiFactorBy4 === 0) {
-          console.log(`${word[i]} gives 2 keystrokes`);
           keystrokes += 2;
         } else if (asciiFactorBy4 === 1) {
-          console.log(`${word[i]} gives 3 keystrokes`);
           keystrokes += 3;
         } else if (asciiFactorBy4 === 2) {
-          console.log(`${word[i]} gives 4 keystrokes`);
           keystrokes += 4;
         } else if (asciiFactorBy4 === 3) {
-          console.log(`${word[i]} gives 5 keystrokes`);
           keystrokes += 5;
         }
       } else {
-        console.log(
-          `${word[i]} with code ${asciiCode} gets into the else block for greater than 118`
-        );
-        if (asciiFactor === 3) {
-          console.log(`${word[i]} gives 2 keystrokes`);
+        if (asciiFactorBy4 === 3) {
           keystrokes += 2;
-        } else if (asciiFactor === 0) {
-          console.log(`${word[i]} gives 3 keystrokes`);
+        } else if (asciiFactorBy4 === 0) {
           keystrokes += 3;
-        } else if (asciiFactor === 1) {
-          console.log(`${word[i]} gives 4 keystrokes`);
+        } else if (asciiFactorBy4 === 1) {
           keystrokes += 4;
-        } else if (asciiFactor === 2) {
-          console.log(`${word[i]} gives 5 keystrokes`);
+        } else if (asciiFactorBy4 === 2) {
           keystrokes += 5;
         }
       }
@@ -64,14 +45,5 @@ const mobileDisplayKeystrokes = (word) => {
   }
   return keystrokes;
 };
-
-// mobileDisplayKeystrokes("adgjmptw")
-// // 111 cutoff
-// mobileDisplayKeystrokes("o");
-// mobileDisplayKeystrokes("pqrs");
-console.log(3+4+4+2+3+4+4+2+3+4+5);
-// mobileDisplayKeystrokes("tuv");
-console.log("\n");
-// mobileDisplayKeystrokes("wxyz");
 
 exports.mobileDisplayKeystrokes = mobileDisplayKeystrokes;
